9c0a506306186725423b717f3ecf64d69851c9ca
wylin <iwylie959@gmail.com>
2025-01-24 23:17:09 -0600

    fix: update Infura API key to be accessible on client-side

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -6,8 +6,8 @@ if (!process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID) {
   throw new Error('Missing NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID');
 }
 
-if (!process.env.INFURA_API_KEY) {
-  throw new Error('Missing INFURA_API_KEY');
+if (!process.env.NEXT_PUBLIC_INFURA_API_KEY) {
+  throw new Error('Missing NEXT_PUBLIC_INFURA_API_KEY');
 }
 
 export const config = getDefaultConfig({
@@ -15,7 +15,7 @@ export const config = getDefaultConfig({
   projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID,
   chains: [mainnet],
   transports: {
-    [mainnet.id]: http(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`)
+    [mainnet.id]: http(`https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY}`)
   },
   ssr: true,
 });
